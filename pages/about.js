eb08123a37904d1e262c3d55d38db447d4852551
Parasbuda <parasbuda1234@gmail.com>
2021-01-04 14:39:12 +0545

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout"
+const about = () => {
+  return (
+    <div>
+    <Layout>
+      <h1>About BitzPrice</h1>
+      <p>Application to view Bitcoin Prices </p>
+      </Layout>
+    </div>
+  );
+};
+export default about;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Fetch from "isomorphic-unfetch"
+import Layout from "../components/Layout"
+const Home = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Welcome to BitzPrice</h1>
+      </Layout>
+    </div>
+  );
+};
+export default Home;
